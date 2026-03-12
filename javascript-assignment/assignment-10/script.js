240921ff5ac9d8f2b83b0f026aaec78fd6202602
khushabudeore22 <khushideore2212@gmail.com>
2026-03-12 19:10:35 +0530

    todo function added in js file

--- javascript-assignment/assignment-10/script.js
+++ javascript-assignment/assignment-10/script.js
@@ -1,30 +1,50 @@
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function addTodo() {
+
+    const input = document.getElementById("todoInput");
+
+    if(input.value === ""){
+        alert("Please enter a task");
+        return;
+    }
+
+    todos.push(input.value);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    displayTodos();
+
+    input.value = "";
+}
+
 function displayTodos(){
 
-const list=document.getElementById("todoList")
+    const list = document.getElementById("todoList");
 
-list.innerHTML=""
+    list.innerHTML = "";
 
-todos.forEach(function(todo,index){
+    todos.forEach(function(todo,index){
 
-let li=document.createElement("li")
+        let li = document.createElement("li");
 
-li.innerHTML = `
-${todo}
-<button onclick="deleteTodo(${index})">Delete</button>
-`
+        li.innerHTML = `
+        ${todo}
+        <button onclick="deleteTodo(${index})">Delete</button>
+        `;
 
-list.appendChild(li)
+        list.appendChild(li);
 
-})
-
+    });
 }
 
 function deleteTodo(index){
 
-todos.splice(index,1)
+    todos.splice(index,1);
 
-localStorage.setItem("todos",JSON.stringify(todos))
+    localStorage.setItem("todos", JSON.stringify(todos));
 
-displayTodos()
-
+    displayTodos();
 }
+
+displayTodos();
